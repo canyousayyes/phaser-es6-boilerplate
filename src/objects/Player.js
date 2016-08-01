1c4bb828079982033aee7fcbf5e9a115288c2784
Fishball Ng <fishballium@gmail.com>
2016-08-01 11:59:11 +0800

    Add basic player object

---
+++ src/objects/Player.js
@@ -0,0 +1,40 @@
+class Player extends Phaser.Sprite {
+    constructor(game, x, y) {
+        super(game, x, y, 'player', 0);
+
+        this._slowSpeed = 200;
+        this._fastSpeed = 400;
+        this._isSlow = false;
+
+        this.game.physics.arcade.enable(this);
+        this.body.collideWorldBounds = true;
+        this.game.stage.addChild(this);
+    }
+
+    get speed() {
+        return this._isSlow ? this._slowSpeed : this._fastSpeed;
+    }
+
+    updateControl() {
+        let cursors = this.game.input.keyboard.createCursorKeys();
+
+        this._isSlow = this.game.input.keyboard.isDown(Phaser.Keyboard.SHIFT);
+
+        this.body.velocity.set(0);
+
+        if (cursors.left.isDown) {
+            this.body.velocity.x -= this.speed;
+        } else if (cursors.right.isDown) {
+            this.body.velocity.x += this.speed;
+        }
+
+        if (cursors.up.isDown) {
+            this.body.velocity.y -= this.speed;
+        } else if (cursors.down.isDown) {
+            this.body.velocity.y += this.speed;
+        }
+    }
+
+}
+
+export default Player;
--- src/states/BattleState.js
+++ src/states/BattleState.js
@@ -1,23 +1,29 @@
+import Player from 'objects/Player';
+
 class BattleState extends Phaser.State {
 
     constructor() {
         super();
         this._bullets = null;
+        this._player = null;
     }
 
     preload() {
         this.game.load.image('red', 'assets/red.png');
+        this.game.load.image('player', 'assets/star.png');
     }
 
     create() {
-        this._bullets = this.game.add.group();
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        this._bullets = this.game.add.group();
         let red = this._bullets.create(100, 200, 'red');
-        red.scale.setTo(1);
+
+        this._player = new Player(this.game, this.game.world.centerX, this.game.world.height - 200);
     }
 
     update() {
-
+        this._player.updateControl();
     }
 }
 
