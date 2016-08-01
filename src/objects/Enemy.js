eecf7a555ed545acc98b5adbfcd7613a9ca0a1a7
Fishball Ng <fishballium@gmail.com>
2016-08-01 19:14:11 +0800

    Set enemy's physics body

--- src/objects/Enemy.js
+++ src/objects/Enemy.js
@@ -4,16 +4,24 @@ class Enemy extends Phaser.Sprite {
     constructor(game, x, y, key, frame) {
         super(game, x, y, key, frame);
 
+        // Set physics body to be center of texture
         this.game.physics.arcade.enable(this);
+        this.body.width = this.body.height = 96;
+        this.body.offset.x = (this.width - this.body.width) / 2;
+        this.body.offset.y = (this.height - this.body.height) / 2;
+
+        // Center align the texture
         this.anchor.set(0.5);
         this.game.world.addChild(this);
         this.game.world.sendToBack(this);
 
+        // Create Bullet for that enemy
         this._bullets = this.game.add.group(this.game.world, 'Bullet', false, true, Phaser.Physics.ARCADE);
         this._bullets.classType = Bullet;
         this._bullets.createMultiple(500, 'red');
         this.game.world.bringToTop(this._bullets);
 
+        // Add time events
         this.game.time.events.loop(Phaser.Timer.SECOND, this.shoot, this);
     }
 
